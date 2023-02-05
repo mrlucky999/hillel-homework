cb20289cb4d4b3b550b9b4d8ddc0af3f05d0d94c
mrlucky999 <rostislav17@ukr.net>
2023-02-06 00:32:16 +0200

    Revert "task46"
    This reverts commit 599d5226a0f5b030723039b34549805bad07f4f9.

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ $tasks = [
             'id' => 1,
             'title' => 'Задача 1',
             'description' => 'Описание задачи 1',
-            'due_date' => '2023-02-10',
+            'due_date' => ('2023-02-05'),
         ],
         ['id' => 2,
             'title' => 'Задача 2',
             'description' => 'Описание задачи 2',
-            'due_date' => '2023-02-13',
+            'due_date' => ('2023-02-12'),
         ],
     ],
     'todo' => [],
@@ -34,18 +34,28 @@ $tasks = [
             ['id' => 3,
             'title' => 'Задача 3',
             'description' => 'Описание задачи 3',
-            'due_date' => '2023-02-07',
-                ]
+            'due_date' => ('tomorrow'),]
     ],
     'done' => [],
 ];
 
 
+function checkfinishtask ($date) {
 
-function getHours($date) {
     $due_date = strtotime($date);
     $current_time = time();
     $difference = $due_date - $current_time;
+    $result = floor($difference / (60*60));
+    if($result > 24) {
+        return true;
+    }
+    return false;
+}
+
+function getHours($date) {
+    $value = strtotime($date);
+    $current = time();
+    $difference = $value - $current;
     return floor($difference / (60*60));
 
 }
